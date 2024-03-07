ca574f999a8778bcd8daccb74d08e9154346547c
Mujtaba Ahmad <muj.ahmad05@gmail.com>
2024-03-07 12:21:41 +0500

    App-opening-confirm

--- Running-Scripts/Application-Opeining.py
+++ Running-Scripts/Application-Opeining.py
@@ -1,15 +1,16 @@
 import os
 
 def open_program(program_name):
+    #Utilize the start menu folder to open the program
     program_path = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\" + program_name + ".lnk"
     
     try:
         os.startfile(program_path)
         print(f"Successfully opened {program_path}")
 
+    #Need to put failure code
     except:
         print("File doesn't exixt")
 
-# Example usage
-program_name = "Google Chrome"
-open_program(program_name)
+if __name__ == "__main__":
+    open_program("Google Chrome")
